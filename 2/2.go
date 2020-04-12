1c4d73e5c876aa2ab68dd3a5c3d38858e2c782c6
Liam <11602015+lgvacanti@users.noreply.github.com>
2020-04-12 18:30:07 +0200

    Complete 2

--- 2/2.go
+++ 2/2.go
@@ -1,9 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
+	start := time.Now()
+
 	var evenFib int
 
+	var a int = 1
+	var b int = 2
+
+	for b < 4000000 {
+		if a%2 == 0 {
+			evenFib += a
+		}
+		a, b = b, a+b
+	}
+	evenFib += a
+
+	elapsed := time.Since(start)
+
 	fmt.Println(evenFib)
+	fmt.Println(elapsed)
 }
