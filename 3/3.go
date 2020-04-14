5501338c2fa404454223f6019c6f0db58ead1397
Liam <11602015+lgvacanti@users.noreply.github.com>
2020-04-14 22:45:16 +0200

    Solved 3

--- 3/3.go
+++ 3/3.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func isPrime(n int) bool {
 	for i := 2; i < n/2+1; i++ {
 		if n%i == 0 {
@@ -13,4 +15,16 @@ func main() {
 
 	n := 600851475143
 
+	for !isPrime(n) {
+
+		for i := 2; i < n; i++ {
+			if n%i == 0 {
+				fmt.Println(i)
+				n = n / i
+				break
+			}
+
+		}
+	}
+	fmt.Println(n)
 }
--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # projecteuler
- 
-1:
- - go
- 
- 2:
- - go
+
+1: go
+2: go
+3: go
